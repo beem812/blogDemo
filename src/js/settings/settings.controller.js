79b4a15457bfdb9b5126095d73723d653779ee72
James Beem <beem132@gmail.com>
2016-06-06 22:45:36 -0500

    added auto fill for the user settings page

--- src/js/settings/settings.controller.js
+++ src/js/settings/settings.controller.js
@@ -1,6 +1,19 @@
 class SettingsCtrl{
-  constructor() {
+  constructor(User){
     'ngInject';
-    
+
+    this.formData = {
+      email: User.current.email,
+      bio: User.current.bio,
+      image: User.current.image,
+      username: User.current.username
+    };
+
+    //Bind is required because the logout method assumes
+    //the execution context is within the User object.
+    this.logout = User.logout.bind(User);
+
   }
 }
+
+export default SettingsCtrl;
